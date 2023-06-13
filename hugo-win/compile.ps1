532c807f92f50e5bdf36cf9338623422bd3503e8
kasusa <kasusaland@gmail.com>
2023-06-14 07:56:07 +0800

    Update compile.ps1

--- hugo-win/compile.ps1
+++ hugo-win/compile.ps1
@@ -1,7 +1,8 @@
 # 保存原始工作目录
 $originalPath = Get-Location
 
-cd ~/Documents/GitHub/cornBlog-bootstraptheme
-hugo -d  ~/Documents/GitHub/cornradio.github.io/hugo
+cd "C:\Users\kasus\Documents\GitHub\cornBlog-bootstraptheme"
+hugo -d "C:\Users\kasus\Documents\GitHub\cornradio.github.io\hugo"
+
 # 恢复原始工作目录
 Set-Location $originalPath
