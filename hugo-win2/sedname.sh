0ae8f7c4073f05f6d22a6d904755f4899b194db4
Name <kasusaland@gmail.com>
2024-05-27 19:10:27 +0800

    Update sedname.sh

--- hugo-win2/sedname.sh
+++ hugo-win2/sedname.sh
@@ -1,4 +1,6 @@
-sed 's/kasus/KK/g' *.ps1
+# https://github.com/bmatzelle/gow/releases/tag/v0.8.0
+
+sed 's/KK/Admin/g' -i *.ps1
 sed 's|C:\\Users\\KK\\Documents\\GitHub\\cornradio.github.io\\hugo|C:\\Users\\KK\\Desktop\\Sout|g' *.ps1 -i
 sed 's|C:\\Users\\KK\\Documents\\GitHub\\cornBlog-bootstraptheme|C:\\Users\\KK\\Documents\\GitHub\\hugo-stacktheme|g' *.ps1 -i
 sed 's|C:/Users/KK/Documents/GitHub/cornBlog-bootstraptheme|C:\\Users\\KK\\Documents\\GitHub\\hugo-stacktheme|g' *.ps1 -i
